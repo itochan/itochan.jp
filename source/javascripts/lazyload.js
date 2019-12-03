63c51f34e49ba173d80fea0ee5f997126e38404f
Kazunori Jo <itochan315@gmail.com>
2019-12-04 04:58:59 +0900

    Stop lazy load CSS

--- source/javascripts/lazyload.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  var stylesheets = document.getElementsByClassName('stylesheet');
-  Array.from(stylesheets).forEach(function (stylesheet) {
-    stylesheet.rel = 'stylesheet';
-  })
-})();
